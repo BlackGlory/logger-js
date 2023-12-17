a548eda6e2f0bcfc6ccb655834dda55423437d08
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-12-17 14:53:07 +0000

    refactor: disable auto reconnect

--- src/logger-client.ts
+++ src/logger-client.ts
@@ -179,6 +179,7 @@ export class LoggerClient {
                   resetHeartbeatTimeout(controller, heartbeatTimeout)
                 }
               }
+            , autoReconnect: false
             }
           )
         ) {
